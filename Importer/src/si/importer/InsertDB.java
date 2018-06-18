17c3981a5c10ffa5321fd3cc2c9002ae6bebdd07
andrej <andrej@ubuntu17-10-VB>
2018-06-18 11:53:05 +0200

    Added autoclose

--- Importer/src/si/importer/InsertDB.java
+++ Importer/src/si/importer/InsertDB.java
@@ -19,7 +19,6 @@ import si.importer.model.FileData;
  */
 public class InsertDB {
 
-	
 	static {
 		try {
 			Class.forName("org.hsqldb.jdbc.JDBCDriver");
@@ -27,9 +26,10 @@ public class InsertDB {
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Get SQL connection.
+	 * 
 	 * @return
 	 * @throws SQLException
 	 */
@@ -37,19 +37,18 @@ public class InsertDB {
 		return DriverManager.getConnection("jdbc:hsqldb:file:db/testdb", "SA", "");
 	}
 
-
 	/**
 	 * Drop table.
 	 * 
 	 * @throws SQLException
 	 */
 	public void dropTabel() throws SQLException {
-		Connection connection = this.getConnection();
-		Statement statement = connection.createStatement();
+		try (Connection connection = this.getConnection(); Statement statement = connection.createStatement()) {
 
-		statement.executeUpdate("DROP TABLE fo_random");
+			statement.executeUpdate("DROP TABLE fo_random");
+		}
 	}
-	
+
 	/**
 	 * Create table.
 	 * 
@@ -57,13 +56,12 @@ public class InsertDB {
 	 * @throws ClassNotFoundException
 	 */
 	public void createTable() throws SQLException, ClassNotFoundException {
-		Connection connection = this.getConnection();
-		Statement statement = connection.createStatement();
+		try (Connection connection = this.getConnection(); Statement statement = connection.createStatement()) {
 
-		statement.executeUpdate("CREATE TABLE fo_random (" + "MATCH_ID BIGINT NOT NULL, MARKET_ID BIGINT NOT NULL,"
-				+ "OUTCOME_ID BIGINT NOT NULL, SPECIFIERS VARCHAR(1000), DATE_INSERT TIMESTAMP NOT NULL " + ");");
+			statement.executeUpdate("CREATE TABLE fo_random (" + "MATCH_ID BIGINT NOT NULL, MARKET_ID BIGINT NOT NULL,"
+					+ "OUTCOME_ID BIGINT NOT NULL, SPECIFIERS VARCHAR(1000), DATE_INSERT TIMESTAMP NOT NULL " + ");");
+		}
 	}
-	
 
 	/**
 	 * Batch for inserting data to DB.
@@ -72,77 +70,86 @@ public class InsertDB {
 	 * @throws Exception
 	 */
 	public void batch(ArrayList<FileData> dataList) throws Exception {
-		Connection connection = this.getConnection();
-		connection.setAutoCommit(false);
-		
-		StringBuffer sql = new StringBuffer();
-		sql.append("INSERT INTO fo_random ");
-		sql.append("(MATCH_ID, MARKET_ID, OUTCOME_ID, SPECIFIERS, DATE_INSERT) ");
-		sql.append("VALUES (?,?,?,?,?) ");
-		PreparedStatement statement = connection.prepareStatement(sql.toString());
-		
-		for(int i=0; i<dataList.size(); i++) {
-			FileData data = dataList.get(i);
-			
-			statement.setLong(1, data.getMatchId());
-			statement.setLong(2, data.getMarketId());
-			statement.setLong(3, data.getOutcomeId());
-			if(data.getSpecifiers() != null) {
-				statement.setString(4, data.getSpecifiers());
-			} else {
-				statement.setNull(4,java.sql.Types.VARCHAR);
-			}
-			
-			statement.setTimestamp(5,getCurrentTimeStamp());
-			statement.addBatch();
-			if(i % 1000 == 0) {
+		try (Connection connection = this.getConnection()) {
+			connection.setAutoCommit(false);
+
+			StringBuffer sql = new StringBuffer();
+			sql.append("INSERT INTO fo_random ");
+			sql.append("(MATCH_ID, MARKET_ID, OUTCOME_ID, SPECIFIERS, DATE_INSERT) ");
+			sql.append("VALUES (?,?,?,?,?) ");
+			try (PreparedStatement statement = connection.prepareStatement(sql.toString())) {
+
+				for (int i = 0; i < dataList.size(); i++) {
+					FileData data = dataList.get(i);
+
+					statement.setLong(1, data.getMatchId());
+					statement.setLong(2, data.getMarketId());
+					statement.setLong(3, data.getOutcomeId());
+					if (data.getSpecifiers() != null) {
+						statement.setString(4, data.getSpecifiers());
+					} else {
+						statement.setNull(4, java.sql.Types.VARCHAR);
+					}
+
+					statement.setTimestamp(5, getCurrentTimeStamp());
+					statement.addBatch();
+					if (i % 1000 == 0) {
+						statement.executeBatch();
+					}
+				}
+
 				statement.executeBatch();
 			}
+			connection.commit();
 		}
-		
-		statement.executeBatch();
-		connection.commit();
-		
 	}
-	
+
 	/**
 	 * Get MIN date_insert from DB.
+	 * 
 	 * @return
 	 * @throws SQLException
 	 */
 	public Date getMinDateInsert() throws SQLException {
-		Connection connection = this.getConnection();
-		Statement statement = connection.createStatement();
-		ResultSet rs = statement.executeQuery("SELECT MIN(DATE_INSERT) FROM fo_random"); 
-		
 		Date date = null;
-		while(rs.next()) {
-			date = new Date(rs.getTimestamp(1).getTime());
+
+		try (Connection connection = this.getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet rs = statement.executeQuery("SELECT MIN(DATE_INSERT) FROM fo_random")) {
+
+			while (rs.next()) {
+				date = new Date(rs.getTimestamp(1).getTime());
+			}
 		}
-		
+
 		return date;
+
 	}
-	
+
 	/**
 	 * Get MAX date_insert from DB.
+	 * 
 	 * @return
 	 * @throws SQLException
 	 */
 	public Date getMaxDateInsert() throws SQLException {
-		Connection connection = this.getConnection();
-		Statement statement = connection.createStatement();
-		ResultSet rs = statement.executeQuery("SELECT MAX(DATE_INSERT) FROM fo_random"); 
-		
 		Date date = null;
-		while(rs.next()) {
-			date = new Date(rs.getTimestamp(1).getTime());
+
+		try (Connection connection = this.getConnection();
+				Statement statement = connection.createStatement();
+				ResultSet rs = statement.executeQuery("SELECT MAX(DATE_INSERT) FROM fo_random")) {
+
+			while (rs.next()) {
+				date = new Date(rs.getTimestamp(1).getTime());
+			}
 		}
-		
+
 		return date;
 	}
 
 	/**
-	 * Helper method for getting current time stamp. 
+	 * Helper method for getting current time stamp.
+	 * 
 	 * @return
 	 */
 	private static java.sql.Timestamp getCurrentTimeStamp() {
@@ -151,6 +158,5 @@ public class InsertDB {
 		return new java.sql.Timestamp(today.getTime());
 
 	}
-
 
 }
